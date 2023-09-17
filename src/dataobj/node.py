9baafc4f8a826375baa68c24be388e2f525d63de
NathanPERIER <periernathan2000@gmail.com>
2023-09-17 17:01:23 +0200

    Added a base Plex media node

---
+++ src/dataobj/node.py
@@ -0,0 +1,15 @@
+
+from .media_record import PlexMediaRecord
+
+from abc import ABC, abstractmethod
+from typing import Sequence
+
+class PlexNode(ABC) :
+
+    @abstractmethod
+    def get_media(self, server_uri: str) -> Sequence[PlexMediaRecord] :
+        pass
+
+    @abstractmethod
+    def get_name(self) -> str :
+        pass
